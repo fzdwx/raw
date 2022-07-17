d39f958b0885dee9712bf06aa6ba1c3746a8e135
fzdwx <likelovec@gmail.com>
2022-07-17 14:18:10 +0800

    fix 高亮

--- src/row.rs
+++ src/row.rs
@@ -323,7 +323,7 @@ impl Row {
             while let Some(search_match) = self.find(word, index, SearchDirection::FORWARD) {
                 if let Some(next_index) = search_match.checked_add(word[..].graphemes(true).count())
                 {
-                    for i in index.saturating_add(search_match)..next_index {
+                    for i in search_match..next_index {
                         self.highlighting[i] = highlighting::Type::Match;
                     }
                     index = next_index;
